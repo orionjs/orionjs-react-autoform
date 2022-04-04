fd57a063fd32d6df8e5b7a678ed07e144690280b
Nicolás López <nicolaslopezj@me.com>
2022-04-04 13:26:37 -0400

    update get field componments

--- src/Fields/getFieldComponent.tsx
+++ src/Fields/getFieldComponent.tsx
@@ -1,8 +1,12 @@
 import {SchemaNode} from '@orion-js/schema'
 import isArray from 'lodash/isArray'
+import {options} from '..'
 
-export default (field: Partial<SchemaNode>, getField: (fieldType: string) => any) => {
+export default (field: Partial<SchemaNode>, getField?: (fieldType: string) => any) => {
   const finalGetField = function (key: string) {
+    if (!getField) {
+      getField = options.getField
+    }
     const field = getField(key)
     if (!field) throw new Error('No field component for type: ' + key)
     return field
